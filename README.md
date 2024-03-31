d8663ec0ed59abb3898bf1431af66914c70b73db
Shamblonaut <shaheembn@gmail.com>
2024-03-31 06:51:22 +0530

    Setup project from template

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# webpack-template
+# ui-exercise
--- webpack.config.js
+++ webpack.config.js
@@ -3,37 +3,37 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
-    },
-    
-    module: {
-        rules: [
-            {
-                test: /\.(html)$/,
-                use: ["html-loader"],
-            },
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/html/index.html",
-            filename: "index.html",
-            inject: "body",
-        }),
-    ],
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
 
-    mode: "development",
-    devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(html)$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/html/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
+
+  mode: "development",
+  devtool: "inline-source-map",
 };
